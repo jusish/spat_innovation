bbed64272d7dc57f6bc766fcbc56ac87f025057e
jusish <ishimwejustin67@gmail.com>
2024-01-22 19:38:56 +0200

    initial commit

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Hero from "../Hero";
-import Who from "../Who";
-import What from "../What";
-import Products from "../Products";
-import Urbannft from "../Urbannft";
-import Faqs from "../Faqs";
+import Hero from "./Hero";
+import Who from "./Who";
+import What from "./What";
+import Products from "./Products";
+import Urbannft from "./Urbannft";
+import Faqs from "./Faqs";
 import Footer from "../Footer";
 
 const Home = () => {
