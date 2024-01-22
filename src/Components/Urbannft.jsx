2246a5d0db3856e9c1a3f0eafe103923f15cda56
jusish <ishimwejustin67@gmail.com>
2024-01-22 13:01:03 +0200

    initial commit

--- src/Components/Urbannft.jsx
+++ src/Components/Urbannft.jsx
@@ -2,8 +2,11 @@ import React from "react";
 
 const Urbannft = () => {
   return (
-    <div>
+    <div className="mx-44">
       <div className="flex mt-20 gap-96">
+        <div>
+          <img src="/assets/desktop.png" alt="jornyman" /> <span><img src="/assets/Ellipse.png" alt="Ellipse" /></span>
+        </div>
         <div className="w-[544.19px]">
           <h1 className=" text-[38.77px] font-[700]">Setroman</h1>
           <p className=" font-[500] text-[25.85px] mt-10">
@@ -21,9 +24,6 @@ const Urbannft = () => {
           <button className="px-6 py-3 mt-8 text-white bg-mycolor rounded-2xl">
             Request Demo
           </button>
-        </div>
-        <div>
-          <img src="/assets/Jornyman.png" alt="jornyman" />
         </div>
       </div>
     </div>
